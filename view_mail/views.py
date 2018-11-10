93096b4ddc0bd887bbfdb423b60a1122304008c0
petro.sernivka <p.sernivka@gmail.com>
2018-11-10 21:56:17 +0200

    modified view_mail/view_mail.html

--- view_mail/views.py
+++ view_mail/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h3>Сторінка view_mail</h3>')
+    return render(request, 'view_mail/view_mail.html')
