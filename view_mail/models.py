48fded844e225968553009983ab8859f4fbb4d40
petro.sernivka <p.sernivka@gmail.com>
2018-11-10 20:48:16 +0200

    created Mails in database

--- view_mail/models.py
+++ view_mail/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Mails(models.Model):
+    date = models.DateTimeField()
+    sender = models.CharField(max_lenght = 50)
+    receiver = models.CharField(max_lenght = 50)
+    send_mode = models.CharField(max_lenght = 50)
+    subject = models.CharField(max_lenght = 100)
+    body = models.TextField()
