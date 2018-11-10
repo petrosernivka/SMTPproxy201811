d527f131b0ada8db264902923f7353b49b454dd0
petro.sernivka <p.sernivka@gmail.com>
2018-11-10 19:23:46 +0200

    added view_mail to settings.py

--- SMTPproxy201811/settings.py
+++ SMTPproxy201811/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mainApp',
     'send_mail',
+    'view_mail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
