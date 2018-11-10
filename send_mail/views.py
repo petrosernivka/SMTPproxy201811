13c266e9e5847bf55d1dfbb530a4bd52ad4d0120
petro.sernivka <p.sernivka@gmail.com>
2018-11-10 21:43:33 +0200

    modified send_mail/send_mail.html

--- send_mail/views.py
+++ send_mail/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h3>Сторінка view_mail</h3>')
+    return render(request, 'send_mail/send_mail.html')
