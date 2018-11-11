4559bd2dd3f32d3793bdc2ed634cf40860a74e09
petro.sernivka <p.sernivka@gmail.com>
2018-11-11 16:04:20 +0200

    added scripts.js on wrapper.html

--- view_mail/models.py
+++ view_mail/models.py
@@ -7,3 +7,6 @@ class Mails(models.Model):
     send_mode = models.CharField(max_length = 50)
     subject = models.CharField(max_length = 100)
     body = models.TextField()
+
+    # def __str__(self):
+    #     return '{}{}{}'.format(self.date, self.sender, self.reseiver)
