f16ee2324c811fb4811c7b58cee623c7089816a9
petro.sernivka <p.sernivka@gmail.com>
2018-11-12 06:42:05 +0200

    deleted send/ and added SMTP-functional to send_mail/views.py

--- SMTPproxy201811/urls.py
+++ SMTPproxy201811/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('mainApp.urls')),
     path('send_mail/', include('send_mail.urls')),
     path('view_mail/', include('view_mail.urls')),
+    path('send/', include('send.urls')),
 ]
