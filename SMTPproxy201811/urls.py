954ad810966eac508c64b435479d0ba99bdf76ab
petro.sernivka <p.sernivka@gmail.com>
2018-11-12 10:25:23 +0200

    modified homePage.html and send_mail.html

--- SMTPproxy201811/urls.py
+++ SMTPproxy201811/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', include('mainApp.urls')),
     path('send_mail/', include('send_mail.urls')),
     path('view_mail/', include('view_mail.urls')),
-    path('send/', include('send.urls')),
 ]
