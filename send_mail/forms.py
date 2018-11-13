c5063258ed73df866812d92cfa43323f25494e30
petro.sernivka <p.sernivka@gmail.com>
2018-11-13 16:25:58 +0200

    created MailForm.save()

--- send_mail/forms.py
+++ send_mail/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Mail
 
 class MailForm(forms.Form):
     date = models.DateTimeField(auto_now_add=True)
@@ -7,3 +8,14 @@ class MailForm(forms.Form):
     send_mode = models.CharField(max_length = 50)
     subject = models.CharField(max_length = 100)
     body = models.TextField()
+
+    def save(self):
+        new_mail = Mail.objects.create(
+            date = self.cleaned_data['date'],
+            sender = self.cleaned_data['sender'],
+            receiver = self.cleaned_data['receiver'],
+            send_mode = self.cleaned_data['send_mode'],
+            subject = self.cleaned_data['subject'],
+            body = self.cleaned_data['body'],
+        )
+        return new_mail
