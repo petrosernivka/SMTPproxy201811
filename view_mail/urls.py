a8385359548c2a25ddee24af16bc474ddfc5257f
petro.sernivka <p.sernivka@gmail.com>
2018-11-13 15:49:39 +0200

    modified view_mail.html

--- view_mail/urls.py
+++ view_mail/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from django.views.generic import ListView, DetailView
-from .views import view_mail
+from .views import *
 
 urlpatterns = [
     path('', view_mail, name='view_mail_url'),
+    path('mails/<sender>/', mails_list, name='mails_list_url'),
 ]
--- view_mail/views.py
+++ view_mail/views.py
@@ -5,6 +5,6 @@ from send_mail.models import Mail
 def view_mail(request):
     return render(request, 'view_mail/view_mail.html')
 
-def mails_list(request, own_mail='donkixot21@outlook.com'):
-    mails = Mail.objects.filter(sender=own_mail)
+def mails_list(request, sender='donkixot21@outlook.com'):
+    mails = Mail.objects.filter(sender=sender)
     return render(request, 'view_mail/mail_list.html', context={'mails': mails})
